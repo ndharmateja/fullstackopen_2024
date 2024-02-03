063808b73141f5d782b6e35883b05222d9a84c79
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-03 14:41:34 +0530

    initialize state app

---
+++ practice/part1/state/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter - 1)}>minus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+    </div>
+  );
+};
+
+export default App;
