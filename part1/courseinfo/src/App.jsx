61fba88aa4d839fd91b40994fa6e708389eb939c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-03 13:35:16 +0530

    solve 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,27 +3,29 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Footer parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Footer parts={course.parts} />
     </div>
   );
 };
