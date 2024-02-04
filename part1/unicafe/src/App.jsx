17c8c6d237c875c57e45d4eba45a5479811df4be
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-04 17:20:46 +0530

    solve 1.6-1.7

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = () => good + neutral + bad;
+  const average = () => (good - bad) / total();
+  const positivePercentage = () => (good * 100) / total();
+
+  return (
+    <div>
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      <div>
+        <h1>statistics</h1>
+        <span>good {good}</span> <br />
+        <span>neutral {neutral}</span> <br />
+        <span>bad {bad}</span> <br />
+        <span>total {total()}</span> <br />
+        <span>average {average()}</span> <br />
+        <span>positive {positivePercentage()} %</span>
+      </div>
+    </div>
+  );
+};
+
+export default App;
