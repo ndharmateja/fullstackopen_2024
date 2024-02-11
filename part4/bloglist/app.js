d7e03d6231a1f1cad41d2fab2524d4f952875111
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-11 20:57:02 +0530

    add logging to mongo db connection

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,12 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/Blog");
 const blogsRouter = require("./routes/blogs");
 require("dotenv").config();
+const logger = require("./utils/logger");
+const mw = require("./utils/middleware");
 
+// Connect to Mongo DB
 const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+logger.info("Connecting to Mongo DB..");
+mongoose
+    .connect(mongoUrl)
+    .then(() => logger.info("Successfully connected to Mongo DB."))
+    .catch((e) =>
+        logger.error(`Failed to connect to Mongo DB. "${e.message}"`)
+    );
 
 app.use(cors());
 app.use(express.json());
