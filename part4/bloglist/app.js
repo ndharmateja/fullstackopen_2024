31bc048ddfd0b7a93ff149b75e3ff4e5fa15b28b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-11 20:48:09 +0530

    refactor into app.js

---
+++ part4/bloglist/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Blog = require("./models/Blog");
+require("dotenv").config();
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs);
+    });
+});
+
+app.post("/api/blogs", (request, response) => {
+    const blog = new Blog(request.body);
+
+    blog.save().then((result) => {
+        response.status(201).json(result);
+    });
+});
+
+module.exports = app;
