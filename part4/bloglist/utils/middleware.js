9fe4d89d303b1fa4a2335ad04107636a1e129f77
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-11 20:41:11 +0530

    add utils files

---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,33 @@
+const morgan = require("morgan");
+
+const notFoundRoute = (req, res) =>
+    res
+        .status(404)
+        .send('<h1>Unknown endpoint</h1><p>Go to <a href="/info">info</a></p>');
+
+const requestLogger = morgan((tokens, req, res) => {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+        JSON.stringify(req.body),
+    ].join(" ");
+});
+
+const errorHandler = (error, _req, res, next) => {
+    console.log(error);
+
+    if (error.name === "CastError") {
+        return res.status(400).json({ error: "malformed id" });
+    } else if (error.name === "ValidationError") {
+        return res.status(400).json({ error: error.message });
+    }
+
+    next(error);
+};
+
+module.exports = { notFoundRoute, requestLogger, errorHandler };
