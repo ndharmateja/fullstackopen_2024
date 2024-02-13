9b725da65c7fceb037fd1994e9ad187183958ca2
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-13 20:15:25 +0530

    solve 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,6 +1,14 @@
-const dummy = (blogs) => 1;
+const dummy = (_blogs) => 1;
 
 const totalLikes = (blogs) =>
     blogs.reduce((likes, currentBlog) => likes + currentBlog.likes, 0);
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) =>
+    blogs.reduce((favBlog, currBlog) => {
+        // if fav blog is undefined (initial value) or
+        // if the curr blog's likes are higher than fav blog's likes
+        // we choose curr blog, otherwise we choose fav blog
+        return !favBlog || currBlog.likes > favBlog.likes ? currBlog : favBlog;
+    }, undefined);
+
+module.exports = { dummy, totalLikes, favoriteBlog };
