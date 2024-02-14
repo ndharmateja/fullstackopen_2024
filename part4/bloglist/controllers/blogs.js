1351fee1faf20564329a5ce09fe9a3f2f77008f3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-14 23:02:33 +0530

    install and use express-async-errors

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -5,19 +5,15 @@ const getAllBlogs = async (_req, res) => {
     return res.json(blogs);
 };
 
-const createBlog = async (req, res, next) => {
+const createBlog = async (req, res) => {
     const blogData = req.body;
 
     // likes should default to 0 if missing in req
     if (!blogData.likes) blogData.likes = 0;
 
     const blog = new Blog(blogData);
-    try {
-        const result = await blog.save();
-        return res.status(201).json(result);
-    } catch (error) {
-        next(error);
-    }
+    const result = await blog.save();
+    return res.status(201).json(result);
 };
 
 module.exports = { getAllBlogs, createBlog };
