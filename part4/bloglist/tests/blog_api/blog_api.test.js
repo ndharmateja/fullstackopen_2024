081d87f92f05080638077ddd1ac484c78a32bf35
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-14 22:03:31 +0530

    solve 4.8

---
+++ part4/bloglist/tests/blog_api/blog_api.test.js
@@ -0,0 +1,33 @@
+const supertest = require("supertest");
+const app = require("../../app");
+const mongoose = require("mongoose");
+const Blog = require("../../models/Blog");
+const { multipleBlogs } = require("./helper");
+
+const api = supertest(app);
+
+beforeEach(async () => {
+    // Delete all database blogs
+    await Blog.deleteMany({});
+
+    // Add all blogs
+    const blogObjects = multipleBlogs.map((b) => new Blog(b));
+    const promises = blogObjects.map((b) => b.save());
+    await Promise.all(promises);
+});
+
+test("blogs are returned as json", async () => {
+    await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+});
+
+test("number of blogs returned", async () => {
+    const blogs = await api.get("/api/blogs");
+    expect(blogs.body).toHaveLength(multipleBlogs.length);
+});
+
+afterAll(async () => {
+    await mongoose.connection.close();
+});
