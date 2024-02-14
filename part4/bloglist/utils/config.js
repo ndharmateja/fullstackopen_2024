27f1a75c0db4b204bb15408569138fda6c0f5964
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-14 12:26:36 +0530

    add cross-env

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
 const process = require("process");
 
+const PROD_ENV = "prod";
+const DEV_ENV = "dev";
+const TEST_ENV = "test";
+
+const isProd = () => process.env.NODE_ENV === PROD_ENV;
+const isDev = () => process.env.NODE_ENV === DEV_ENV;
+const isTest = () => process.env.NODE_ENV === TEST_ENV;
+
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = isTest()
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = {
     PORT,
