de572232191e62bf916953538444f0a01b87da75
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-15 19:36:54 +0530

    refactor error handling mw

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,4 +1,5 @@
 const logger = require("./logger");
+const BlogAppError = require("../errors/BlogAppError");
 
 const notFoundRoute = (_req, res) =>
     res
@@ -13,14 +14,28 @@ const requestLogger = (req, _res, next) => {
     next();
 };
 
+const isUsernameDuplicate = (error) =>
+    error.name === "MongoServerError" &&
+    error.message.includes("E11000 duplicate key error collection");
+
 const errorHandler = (error, _req, res, next) => {
     logger.info(error);
 
-    if (error.name === "CastError") {
-        return res.status(400).json({ error: "malformed id" });
-    } else if (error.name === "ValidationError") {
+    // Mongoose invalid id error
+    if (error.name === "CastError")
+        return res.status(400).json({ error: "malformed `id`" });
+
+    // Mongoose validation error
+    if (error.name === "ValidationError")
         return res.status(400).json({ error: error.message });
-    }
+
+    // Duplicate username error
+    if (isUsernameDuplicate(error))
+        return res.status(400).json({ error: "`username` should be unique" });
+
+    // Internal app error
+    if (error instanceof BlogAppError)
+        return res.status(error.status).json({ error: error.message });
 
     next(error);
 };
