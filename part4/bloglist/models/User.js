aea676c5a107a09371d96673be866e54bed8fa7d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-16 01:22:37 +0530

    add blogs reference to user schema

--- part4/bloglist/models/User.js
+++ part4/bloglist/models/User.js
@@ -16,12 +16,12 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     },
-    // blogs: [
-    //     {
-    //         type: mongoose.Schema.Types.ObjectId,
-    //         ref: "Blog",
-    //     },
-    // ],
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Blog",
+        },
+    ],
 });
 
 userSchema.set("toJSON", {
