9697130f6931abe1fcc4837e324d0677d5b23f20
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-15 19:36:12 +0530

    add controller for get and post users

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -1,9 +1,29 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const BlogAppError = require("../errors/BlogAppError");
+
 const getAllUsers = async (_req, res) => {
-    return res.json({ route: "get all users" });
+    const users = await User.find({});
+    return res.json(users);
 };
 
-const createUser = async (_req, res) => {
-    return res.json({ route: "create user" });
+const createUser = async (req, res) => {
+    const { username, name, password } = req.body;
+
+    // If password is missing or less than 3 chars, throw error
+    if (!password) throw new BlogAppError(400, "`password` is required");
+    if (password.length < 3)
+        throw new BlogAppError(400, "`password` should be atleast 3 chars");
+
+    // create hash
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    // create User
+    const user = new User({ username, name, passwordHash });
+    const savedUser = await user.save();
+
+    return res.status(201).json(savedUser);
 };
 
 const getOneUser = async (req, res) => {
