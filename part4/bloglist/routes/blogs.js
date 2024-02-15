f703c6c961ae2f82daa6ec0d3862fff392910061
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-15 18:55:39 +0530

    add route comments

--- part4/bloglist/routes/blogs.js
+++ part4/bloglist/routes/blogs.js
@@ -7,8 +7,13 @@ const {
     updateBlog,
 } = require("../controllers/blogs");
 
+// Create blogs router
 const blogsRouter = express.Router();
+
+// Route /api/blogs
 blogsRouter.route("/").get(getAllBlogs).post(createBlog);
+
+// Route /api/blogs/:id
 blogsRouter.route("/:id").get(getOneBlog).put(updateBlog).delete(deleteBlog);
 
 module.exports = blogsRouter;
