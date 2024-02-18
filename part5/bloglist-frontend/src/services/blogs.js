54922478a7cc6932ddad72c228f3d716f1053ba7
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-18 16:56:12 +0530

    add login service

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,15 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios";
+const blogsUrl = "/api/blogs";
+const loginUrl = "/api/login";
+
+const login = (username, password) => {
+    const request = axios.post(loginUrl, { username, password });
+    return request.then((response) => response.data);
+};
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+    const request = axios.get(blogsUrl);
+    return request.then((response) => response.data);
+};
 
-export default { getAll }
+export default { login, getAll };
