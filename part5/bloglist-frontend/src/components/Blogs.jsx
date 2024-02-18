8241ca31b7dff2cb3f6dcdbfbd7c0f6423fea359
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-18 16:56:06 +0530

    refactor into Blogs component

---
+++ part5/bloglist-frontend/src/components/Blogs.jsx
@@ -0,0 +1,15 @@
+import Blog from "./Blog";
+
+const Blogs = ({ blogs, name }) => {
+    return (
+        <div>
+            <h2>blogs</h2>
+            <p>{name} logged in</p>
+            {blogs.map((blog) => (
+                <Blog key={blog.id} blog={blog} />
+            ))}
+        </div>
+    );
+};
+
+export default Blogs;
