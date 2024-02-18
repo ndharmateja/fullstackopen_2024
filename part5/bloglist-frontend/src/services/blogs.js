98462be5bc977072854c6d7ccc646cc79a3a8d15
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-18 17:38:22 +0530

    add set token fn to service

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -2,6 +2,10 @@ import axios from "axios";
 const blogsUrl = "/api/blogs";
 const loginUrl = "/api/login";
 
+let token = null;
+
+const setToken = (t) => (token = `Bearer ${t}`);
+
 const login = async (username, password) => {
     const { data } = await axios.post(loginUrl, { username, password });
     return data;
