393fb9574db88ba8985cd44835210f66f9ec1253
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-18 17:38:45 +0530

    remove heading

--- part5/bloglist-frontend/src/components/Blogs.jsx
+++ part5/bloglist-frontend/src/components/Blogs.jsx
@@ -3,6 +3,7 @@ import Blog from "./Blog";
 const Blogs = ({ blogs, name, onLogoutClick }) => {
     return (
         <div>
+            <h2>blogs</h2>
             {blogs.map((blog) => (
                 <Blog key={blog.id} blog={blog} />
             ))}
--- part5/bloglist-frontend/src/components/Header.jsx
+++ part5/bloglist-frontend/src/components/Header.jsx
@@ -1,7 +1,6 @@
 const Header = ({ name, onLogoutClick }) => {
     return (
         <div>
-            <h2>blogs</h2>
             <p>
                 {name} logged in <button onClick={onLogoutClick}>logout</button>
             </p>
