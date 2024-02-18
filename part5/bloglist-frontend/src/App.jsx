20d7e547e4c62e94b67f4cc622ddbf2e41a15095
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-18 17:53:59 +0530

    solve 5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,10 +5,15 @@ import Blogs from "./components/Blogs";
 import { BLOG_APP_USER } from "./constants";
 import Header from "./components/Header";
 import CreateBlogForm from "./components/CreateBlogForm";
+import Notification from "./components/Notification";
 
 const App = () => {
     const [blogs, setBlogs] = useState([]);
     const [user, setUser] = useState(null);
+    const [notification, setNotification] = useState({
+        message: null,
+        isError: false,
+    });
 
     useEffect(() => {
         blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -23,12 +28,32 @@ const App = () => {
         }
     }, []);
 
-    const login = async (username, password) => {
-        const fetchedUser = await blogService.login(username, password);
+    const showNotification = (message, isError) => {
+        setNotification({ message, isError });
+        setTimeout(() => {
+            clearNotification();
+        }, 5000);
+    };
 
-        // Store to local storage
-        window.localStorage.setItem(BLOG_APP_USER, JSON.stringify(fetchedUser));
-        setUser(fetchedUser);
+    const clearNotification = () =>
+        setNotification({ ...notification, message: null });
+
+    const login = async (username, password) => {
+        try {
+            const fetchedUser = await blogService.login(username, password);
+
+            // Store to local storage
+            window.localStorage.setItem(
+                BLOG_APP_USER,
+                JSON.stringify(fetchedUser)
+            );
+            setUser(fetchedUser);
+        } catch (error) {
+            showNotification(error.response.data.error, true);
+
+            // throw error for the child component
+            throw new Error("login failed");
+        }
     };
 
     const handleLogout = () => {
@@ -42,14 +67,22 @@ const App = () => {
 
         try {
             const newBlog = await blogService.createBlog(title, author, url);
+            showNotification(`a new blog "${title}" by "${author}" added`);
             setBlogs([...blogs, newBlog]);
         } catch (error) {
-            // set notification
+            showNotification(error.response.data.error, true);
+
+            // throw error for the child component
+            throw new Error("post creation failed");
         }
     };
 
     return (
         <div>
+            <Notification
+                message={notification.message}
+                isError={notification.isError}
+            />
             {user === null ? (
                 <LoginForm login={login} />
             ) : (
