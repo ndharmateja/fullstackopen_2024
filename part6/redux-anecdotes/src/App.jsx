2ca861524c31ab23f8ec1e25e5f5361817f280f6
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-27 23:36:13 +0530

    solve 6.3

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,34 +1,33 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
+import { voteAnecdote } from "./reducers/anecdoteReducer";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+    const anecdotes = useSelector((state) => state);
+    const dispatch = useDispatch();
 
-  const vote = (id) => {
-    console.log('vote', id)
-  }
+    const vote = (id) => dispatch(voteAnecdote(id));
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes.map((anecdote) => (
+                <div key={anecdote.id}>
+                    <div>{anecdote.content}</div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            ))}
+            <h2>create new</h2>
+            <form>
+                <div>
+                    <input />
+                </div>
+                <button>create</button>
+            </form>
         </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
+    );
+};
 
-export default App
+export default App;
