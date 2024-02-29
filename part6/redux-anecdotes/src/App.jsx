ebe6d4369bde0a859c52473450a94d7a1172fdf2
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-29 21:13:03 +0530

    load anecdotes from db

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,8 +2,22 @@ import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { useEffect } from "react";
+import anecdotesService from "./services/anecdotes";
+import { useDispatch } from "react-redux";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+import { showAndHideNotification } from "./reducers/notificationReducer";
 
 const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        anecdotesService
+            .getAllAnecdotes()
+            .then((anecdotes) => dispatch(setAnecdotes(anecdotes)))
+            .catch((e) => dispatch(showAndHideNotification(e.message)));
+    });
+
     return (
         <div>
             <Notification />
