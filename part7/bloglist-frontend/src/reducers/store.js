1363620026940c2b92687963c028df36924f9c6f
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-03-13 15:51:22 +0530

    init user reducer

--- part7/bloglist-frontend/src/reducers/store.js
+++ part7/bloglist-frontend/src/reducers/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import notificationReducer from "./notificationReducer";
 import blogsReducer from "./blogsReducer";
 import visibilityReducer from "./visibilityReducer";
+import userReducer from "./userReducer";
 
 const store = configureStore({
     reducer: {
         notification: notificationReducer,
         blogs: blogsReducer,
         visible: visibilityReducer,
+        user: userReducer,
     },
 });
 
