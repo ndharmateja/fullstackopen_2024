0051c2ff89bf50dde3a88023389b98bf55fd4332
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-03-13 15:01:34 +0530

    move like blog functionality to redux

--- part7/bloglist-frontend/src/reducers/blogsReducer.js
+++ part7/bloglist-frontend/src/reducers/blogsReducer.js
@@ -8,16 +8,43 @@ const blogsSlice = createSlice({
         setBlogs(_state, action) {
             return action.payload;
         },
+        increaseLikes(state, action) {
+            const id = action.payload;
+            return state.map((b) =>
+                b.id === id ? { ...b, likes: b.likes + 1 } : b
+            );
+        },
     },
 });
 
-const { setBlogs } = blogsSlice.actions;
+const { setBlogs, increaseLikes } = blogsSlice.actions;
 
 export const initializeBlogs = () => {
     return async (dispatch) => {
         const blogs = await blogsService.getAll();
 
         dispatch(setBlogs(blogs));
+    };
+};
+
+export const likeBlog = (id) => {
+    return async (dispatch, getState) => {
+        // Update likes locally
+        dispatch(increaseLikes(id));
+
+        // Find appropriate blog
+        const { blogs } = getState();
+        const blog = blogs.find((b) => b.id === id);
+
+        // Create payload
+        const blogCopy = {
+            ...blog,
+            user: blog.user.id,
+        };
+        delete blogCopy.id;
+
+        // Update like in backend
+        await blogsService.updateBlog(id, blogCopy);
     };
 };
 
