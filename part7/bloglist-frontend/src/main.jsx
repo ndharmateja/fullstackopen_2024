2f3f6636d966c5393662bfc6dca4e1e4e9bc12fe
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-03-13 14:07:03 +0530

    configure store

--- part7/bloglist-frontend/src/main.jsx
+++ part7/bloglist-frontend/src/main.jsx
@@ -1,4 +1,9 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
