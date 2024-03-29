122858b830881c0a3823a3016bfe3af824e72de7
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-03-29 17:42:52 +0530

    solve 9.1

---
+++ part9/bmi/bmiCalculator.ts
@@ -0,0 +1,14 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi = weight / ((height / 100) ^ 2);
+
+    if (bmi < 16) return "Underweight (Severe thinness)";
+    if (bmi < 17) return "Underweight (Moderate thinness)";
+    if (bmi < 18.5) return "Underweight (Mild thinness)";
+    if (bmi < 25) return "Normal (healthy weight)";
+    if (bmi < 30) return "Overweight (Pre-obese)";
+    if (bmi < 35) return "Obese (Class I)";
+    if (bmi < 40) return "Obese (Class II)";
+    return "Obese (Class III)";
+};
+
+console.log(calculateBmi(180, 74));
