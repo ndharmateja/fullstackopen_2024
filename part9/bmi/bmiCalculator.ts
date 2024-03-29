0b4e3648f71de99c8996d4d7f144e78999c5591a
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-03-29 17:54:37 +0530

    add code to parse input bmi

--- part9/bmi/bmiCalculator.ts
+++ part9/bmi/bmiCalculator.ts
@@ -1,4 +1,23 @@
-const calculateBmi = (height: number, weight: number): string => {
+interface BmiInput {
+    height: number;
+    weight: number;
+}
+
+const parseArgs = (args: string[]): BmiInput => {
+    if (args.length < 4) throw new Error("not enough args");
+    if (args.length > 4) throw new Error("too many args");
+
+    const height = Number(args[2]);
+    const weight = Number(args[3]);
+
+    if (isNaN(height) || isNaN(weight))
+        throw new Error("args should be numbers");
+
+    return { height, weight };
+};
+
+const calculateBmi = (input: BmiInput): string => {
+    const { height, weight } = input;
     const bmi = weight / ((height / 100) ^ 2);
 
     if (bmi < 16) return "Underweight (Severe thinness)";
@@ -11,4 +30,11 @@ const calculateBmi = (height: number, weight: number): string => {
     return "Obese (Class III)";
 };
 
-console.log(calculateBmi(180, 74));
+try {
+    const input = parseArgs(process.argv);
+    console.log(calculateBmi(input));
+} catch (error) {
+    let errorMessage = "Error";
+    if (error instanceof Error) errorMessage += `: ${error.message}`;
+    console.error(errorMessage);
+}
